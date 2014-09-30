e22fc73b591b070af88081600c8fc446b8176b3e
Mavi Puri <mavina@gmail.com>
2014-09-30 12:24:40 -0400

    * Add ply scripts

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var auth = require('./routes/auth');
 var trips = require('./routes/trips');
 
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.get('/login', auth.login);
 app.get('/authorize', auth.authorize);
 app.get('/trips', trips.trips);
